6b803ae577cc7b16bfd3beef12038a28c1954106
Satvick Acharya <satvickacharya16@gmail.com>
2021-10-01 14:39:52 -0400

    x

--- format_data.py
+++ format_data.py
@@ -7,7 +7,7 @@ Lines = file.readlines()
 with open("formatted_data.txt", "w") as f:
     for line in Lines:
         thing = list(line.split())
-        x_over_len = float(float(thing[1]) / 635.5)
+        x_over_len = float(float(thing[1]) / 624.0)
         theta = (math.asin(x_over_len) * 180 / math.pi) - 0.63
 
         f.write(str(thing[0]) + " " +
